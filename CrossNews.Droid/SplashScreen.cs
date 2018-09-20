4ac1dcac41b42809eabec050b10420cbccf08c29
Fabio Di Peri <fabiodiperi@outlook.com>
2018-09-21 00:47:50 +0200

    :heavy_plus_sign: Add MvvmCross

---
+++ CrossNews.Droid/SplashScreen.cs
@@ -0,0 +1,14 @@
+﻿using Android.App;
+using Android.Content.PM;
+using CrossNews.Core;
+using MvvmCross.Droid.Support.V7.AppCompat;
+using MvvmCross.Platforms.Android.Core;
+using MvvmCross.Platforms.Android.Views;
+
+namespace CrossNews.Droid
+{
+    [Activity(Label = "@string/app_name", MainLauncher = true, NoHistory = true, ScreenOrientation = ScreenOrientation.Portrait, Theme = "@style/AppTheme")]
+    public class SplashScreen : MvxSplashScreenAppCompatActivity<MvxAppCompatSetup<App>, App>
+    {
+    }
+}
---
+++ CrossNews.Droid/Views/HomeView.cs
@@ -0,0 +1,17 @@
+﻿using Android.App;
+using CrossNews.Core.ViewModels;
+using MvvmCross.Droid.Support.V7.AppCompat;
+using MvvmCross.Platforms.Android.Presenters.Attributes;
+
+namespace CrossNews.Droid.Views
+{
+    [MvxActivityPresentation]
+    [Activity]
+    public class HomeView : MvxAppCompatActivity<HomeViewModel>
+    {
+        protected override void OnViewModelSet()
+        {
+            SetContentView(Resource.Layout.home_view);
+        }
+    }
+}
