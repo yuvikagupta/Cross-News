1628942e31e20e331cccb278d175b6a1968bf2c6
Fabio Di Peri <fabiodiperi@outlook.com>
2018-09-21 19:24:38 +0200

    :man_facepalming: Add missing Core project

---
+++ CrossNews.Core/App.cs
@@ -0,0 +1,19 @@
+﻿using CrossNews.Core.ViewModels;
+using MvvmCross.IoC;
+using MvvmCross.ViewModels;
+
+namespace CrossNews.Core
+{
+    public class App : MvxApplication
+    {
+        public override void Initialize()
+        {
+            CreatableTypes()
+                .EndingWith("Service")
+                .AsInterfaces()
+                .RegisterAsLazySingleton();
+
+            RegisterAppStart<HomeViewModel>();
+        }
+    }
+}
