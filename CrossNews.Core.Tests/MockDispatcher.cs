870cde64e23555cb3b16ecbc72367fc4dc53454e
Fabio Di Peri <fabiodiperi@outlook.com>
2018-10-23 08:44:42 +0200

    :construction: Test supporting classes

---
+++ CrossNews.Core.Tests/MockDispatcher.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using MvvmCross.Base;
+using MvvmCross.ViewModels;
+using MvvmCross.Views;
+
+namespace CrossNews.Core.Tests
+{
+    public class MockDispatcher : MvxMainThreadDispatcher, IMvxViewDispatcher
+    {
+        public readonly List<MvxViewModelRequest> Requests = new List<MvxViewModelRequest>();
+
+        public override bool RequestMainThreadAction(Action action, bool maskExceptions = true)
+        {
+            action();
+            return true;
+        }
+
+        public Task ExecuteOnMainThreadAsync(Action action, bool maskExceptions = true)
+        {
+            action();
+            return Task.CompletedTask;
+        }
+
+        public Task ExecuteOnMainThreadAsync(Func<Task> action, bool maskExceptions = true)
+        {
+            return action();
+        }
+
+        public override bool IsOnMainThread { get; } = true;
+
+        public Task<bool> ShowViewModel(MvxViewModelRequest request)
+        {
+            Requests.Add(request);
+            return Task.FromResult(true);
+        }
+
+        public Task<bool> ChangePresentation(MvxPresentationHint hint)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CrossNews.Core.Tests/ViewModels/ViewModelFixtureBase.cs
@@ -0,0 +1,20 @@
+﻿using MvvmCross.Base;
+using MvvmCross.Tests;
+using MvvmCross.Views;
+
+namespace CrossNews.Core.Tests.ViewModels
+{
+    public abstract class ViewModelFixtureBase : MvxIoCSupportingTest
+    {
+        private MockDispatcher MockDispatcher { get; set; }
+
+        protected ViewModelFixtureBase()
+        {
+            Setup();
+            MockDispatcher = new MockDispatcher();
+            Ioc.RegisterSingleton<IMvxViewDispatcher>(MockDispatcher);
+            Ioc.RegisterSingleton<IMvxMainThreadDispatcher>(MockDispatcher);
+            Ioc.RegisterSingleton<IMvxMainThreadAsyncDispatcher>(MockDispatcher);
+        }
+    }
+}
